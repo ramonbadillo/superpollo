015c6fbfe8c663d11eef0db9f248e5cf965bc564
Ramon Octavio Badillo Rivas <robr8@hotmail.com>
2020-01-20 01:29:37 -0600

    first commit

---
+++ js/gmaps.js
@@ -0,0 +1,74 @@
+"use strict";
+
+function initialize() {
+    var mapCanvas = document.getElementById('map');
+
+    var mapOptions = {
+            center: new google.maps.LatLng(32.2677428, -102.6826007),
+            disableDefaultUI: true,
+            zoom: 9,
+            mapTypeId: google.maps.MapTypeId.ROADMAP,
+            scrollwheel: true
+        }
+        //Create map
+    var map = new google.maps.Map(mapCanvas, mapOptions);
+
+    //Seminole
+    var marker = new google.maps.Marker({
+        position: new google.maps.LatLng(32.707793, -102.645344),
+        map: map,
+        title: 'SuperPollo',
+        icon: 'images/map-marker.png'
+    });
+    //Andrews
+    var marker = new google.maps.Marker({
+        position: new google.maps.LatLng(32.337216, -102.551882),
+        map: map,
+        title: 'SuperPollo',
+        icon: 'images/map-marker.png'
+    });
+    //Downtown
+    var marker = new google.maps.Marker({
+        position: new google.maps.LatLng(31.850472, -102.372112),
+        map: map,
+        title: 'SuperPollo',
+        icon: 'images/map-marker.png'
+    });
+    //BillyHext
+    var marker = new google.maps.Marker({
+        position: new google.maps.LatLng(31.904758, -102.302947),
+        map: map,
+        title: 'SuperPollo',
+        icon: 'images/map-marker.png'
+    });
+
+    // //Create ribbon
+    // var ribbon = new google.maps.Marker({
+    //     position: new google.maps.LatLng(40.755585, -73.981585),
+    //     map: map,
+    //     icon: 'images/map-ribbon.png'
+    // });
+
+    //Map marker info
+    var contentString = '<div id="map-info">' +
+        '<h2>Mozzarella</h2>' +
+        '<p style="text-align:left; margin:0;"><strong>Mozzarella</strong>, has the <strong>best food</strong>aa </p>' +
+        '</div>';
+
+    //Add info to marker 
+    var infowindow = new google.maps.InfoWindow({
+        content: contentString
+    });
+
+    google.maps.event.addListener(marker, 'click', function() {
+        infowindow.open(map, marker);
+    });
+
+    //Keep map centered
+    google.maps.event.addDomListener(window, 'resize', function() {
+        var center = map.getCenter();
+        google.maps.event.trigger(map, "resize");
+        map.setCenter(center);
+    });
+}
+google.maps.event.addDomListener(window, 'load', initialize);
